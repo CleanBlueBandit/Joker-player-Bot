accbc773cfaa71b6999e35f757d195b463e46556
CleanBlueBandit <122689110+CleanBlueBandit@users.noreply.github.com>
2026-04-14 20:40:07 +0400

    Add Index property to Card instances in deck

--- Joker player v2/Program.cs
+++ Joker player v2/Program.cs
@@ -1,4 +1,5 @@
-﻿using Joker_player_v2;
+using Joker_player_v2;
+using System.Linq;
 
 
 #region Initialisation
@@ -8,50 +9,50 @@ string[] names = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen"
 List<Card> deck = new List<Card>
 {
     // Hearts
-    new Card { Name = "6", Suit = "Hearts" },
-    new Card { Name = "7", Suit = "Hearts" },
-    new Card { Name = "8", Suit = "Hearts" },
-    new Card { Name = "9", Suit = "Hearts" },
-    new Card { Name = "10", Suit = "Hearts" },
-    new Card { Name = "Jack", Suit = "Hearts" },
-    new Card { Name = "Queen", Suit = "Hearts" },
-    new Card { Name = "King", Suit = "Hearts" },
-    new Card { Name = "Ace", Suit = "Hearts" },
+    new Card { Name = "6", Suit = "Hearts", Index = 1 },
+    new Card { Name = "7", Suit = "Hearts", Index = 2 },
+    new Card { Name = "8", Suit = "Hearts", Index = 3 },
+    new Card { Name = "9", Suit = "Hearts", Index = 4 },
+    new Card { Name = "10", Suit = "Hearts", Index = 5 },
+    new Card { Name = "Jack", Suit = "Hearts", Index = 6 },
+    new Card { Name = "Queen", Suit = "Hearts", Index = 7 },
+    new Card { Name = "King", Suit = "Hearts", Index = 8 },
+    new Card { Name = "Ace", Suit = "Hearts", Index = 9 },
 
     // Diamonds
-    new Card { Name = "6", Suit = "Diamonds" },
-    new Card { Name = "7", Suit = "Diamonds" },
-    new Card { Name = "8", Suit = "Diamonds" },
-    new Card { Name = "9", Suit = "Diamonds" },
-    new Card { Name = "10", Suit = "Diamonds" },
-    new Card { Name = "Jack", Suit = "Diamonds" },
-    new Card { Name = "Queen", Suit = "Diamonds" },
-    new Card { Name = "King", Suit = "Diamonds" },
-    new Card { Name = "Ace", Suit = "Diamonds" },
+    new Card { Name = "6", Suit = "Diamonds", Index = 1 },
+    new Card { Name = "7", Suit = "Diamonds", Index = 2 },
+    new Card { Name = "8", Suit = "Diamonds", Index = 3 },
+    new Card { Name = "9", Suit = "Diamonds", Index = 4 },
+    new Card { Name = "10", Suit = "Diamonds", Index = 5 },
+    new Card { Name = "Jack", Suit = "Diamonds", Index = 6 },
+    new Card { Name = "Queen", Suit = "Diamonds", Index = 7 },
+    new Card { Name = "King", Suit = "Diamonds", Index = 8 },
+    new Card { Name = "Ace", Suit = "Diamonds", Index = 9 },
 
     // Clubs
-    new Card { Name = "7", Suit = "Clubs" },
-    new Card { Name = "8", Suit = "Clubs" },
-    new Card { Name = "9", Suit = "Clubs" },
-    new Card { Name = "10", Suit = "Clubs" },
-    new Card { Name = "Jack", Suit = "Clubs" },
-    new Card { Name = "Queen", Suit = "Clubs" },
-    new Card { Name = "King", Suit = "Clubs" },
-    new Card { Name = "Ace", Suit = "Clubs" },
+    new Card { Name = "7", Suit = "Clubs", Index = 2 },
+    new Card { Name = "8", Suit = "Clubs", Index = 3 },
+    new Card { Name = "9", Suit = "Clubs", Index = 4 },
+    new Card { Name = "10", Suit = "Clubs", Index = 5 },
+    new Card { Name = "Jack", Suit = "Clubs", Index = 6 },
+    new Card { Name = "Queen", Suit = "Clubs", Index = 7 },
+    new Card { Name = "King", Suit = "Clubs", Index = 8 },
+    new Card { Name = "Ace", Suit = "Clubs", Index = 9 },
 
     // Spades
-    new Card { Name = "7", Suit = "Spades" },
-    new Card { Name = "8", Suit = "Spades" },
-    new Card { Name = "9", Suit = "Spades" },
-    new Card { Name = "10", Suit = "Spades" },
-    new Card { Name = "Jack", Suit = "Spades" },
-    new Card { Name = "Queen", Suit = "Spades" },
-    new Card { Name = "King", Suit = "Spades" },
-    new Card { Name = "Ace", Suit = "Spades" },
+    new Card { Name = "7", Suit = "Spades", Index = 2 },
+    new Card { Name = "8", Suit = "Spades", Index = 3 },
+    new Card { Name = "9", Suit = "Spades", Index = 4 },
+    new Card { Name = "10", Suit = "Spades", Index = 5 },
+    new Card { Name = "Jack", Suit = "Spades", Index = 6 },
+    new Card { Name = "Queen", Suit = "Spades", Index = 7 },
+    new Card { Name = "King", Suit = "Spades" , Index = 8},
+    new Card { Name = "Ace", Suit = "Spades", Index = 9 },
 
     // Jokers
-    new Card { Name = "Joker", Suit = "Red" },
-    new Card { Name = "Joker", Suit = "Black" }
+    new Card { Name = "Joker", Suit = "Red", Index = 10 },
+    new Card { Name = "Joker", Suit = "Black", Index = 10 }
 };
 
 #endregion
@@ -61,38 +62,69 @@ List<Card> deck = new List<Card>
 
 bool validate(Card card)
 {
-    foreach(var c in deck)
-    {
-        if (c.Name == card.Name) goto suit;
-    }
-    return false;
-suit: foreach(var c in deck)
-    {
-        if (c.Suit == card.Suit) return true;
-    }
-    return false;
+    return deck.Any(c => c.Name == card.Name && c.Suit == card.Suit);
 }
 
 List<Card> getHand(int amount)
 {
+    // Initialise
     var final = new List<Card>();
-    for(int i = 0; i < amount; i++)
+    for (int i = 0; i < amount; i++)
     {
-        var c = new Card();
-        Console.Write($"Card number {i+1} -----\nName: ");
-        c.Name = Console.ReadLine();
+        // Get the Card
+        Console.Write($"Card number {i + 1} -----\nName: ");
+        string inputName = Console.ReadLine();
         Console.Write("Suit: ");
-        c.Suit = Console.ReadLine();
-        if (validate(c)) final.Add(c);
-        else throw new Exception(message: "No such card exists");
+        string inputSuit = Console.ReadLine();
+
+        // Find (validate)
+        Card validCard = deck.FirstOrDefault(c => c.Name == inputName && c.Suit == inputSuit);
+
+        // Add
+        if (validCard != null)
+        {
+            final.Add(validCard);
+        }
+        else
+        {
+            Console.WriteLine("No such card exists in this deck. Try again.");
+            i--;
+        }
     }
     return final;
+}
+
+Card? compare(Card c1, Card c2, string Trump)
+{
+    // Jokers
+    if (c1.Name == "Joker" && c2.Name == "Joker") return null;
+    if (c1.Name == "Joker") return c1;
+    if (c2.Name == "Joker") return c2;
+
+    // Same suit
+    if (c1.Suit == c2.Suit)
+    {
+        if (c1.Index == c2.Index) return null;
+        return c1.Index > c2.Index ? c1 : c2;
+    }
+
+    // Trumps
+    if (c1.Suit == Trump) return c1;
+    if (c2.Suit == Trump) return c2;
+
+    // No trumps
+    if (c1.Index == c2.Index) return null;
+    return c1.Index > c2.Index ? c1 : c2;
+}
+
+int placeBet(List<Card> hand)
+{
+    for(int i = 0; i < hand.Count(); i++)
+    {
+
+    }
 }
 
 #endregion
 
 
-foreach(var c in getHand(3))
-{
-    Console.WriteLine(c.write());
-}
